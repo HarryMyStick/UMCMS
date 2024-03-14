afbe8467362bb372ad1572f8cb8b04fee312019b
HarryMyStick <harryson1134@gmail.com>
2024-03-14 22:08:23 +0700

    adding more color in statistic

--- Frontend/src/components/administrator.tsx
+++ Frontend/src/components/administrator.tsx
@@ -257,13 +257,16 @@ const Administrator: React.FC<NavProps> = ({ userId }) => {
                 'rgba(255, 99, 132, 0.6)',
                 'rgba(54, 162, 235, 0.6)',
                 'rgba(255, 206, 86, 0.6)',
-                // Add more colors as needed
+                'rgba(75, 192, 192, 0.6)',
+                'rgba(153, 102, 255, 0.6)',
               ],
+              
               borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                // Add more colors as needed
+                'rgba(255, 99, 132, 0.6)',
+                'rgba(54, 162, 235, 0.6)',
+                'rgba(255, 206, 86, 0.6)',
+                'rgba(75, 192, 192, 0.6)',
+                'rgba(153, 102, 255, 0.6)',
               ],
               borderWidth: 1,
             }]
@@ -304,8 +307,9 @@ const Administrator: React.FC<NavProps> = ({ userId }) => {
                 'rgba(255, 99, 132, 0.6)',
                 'rgba(54, 162, 235, 0.6)',
                 'rgba(255, 206, 86, 0.6)',
-                // Add more colors as needed
-              ],
+                'rgba(75, 192, 192, 0.6)',
+                'rgba(153, 102, 255, 0.6)',
+              ],              
               hoverOffset: 4
             }]
           }
